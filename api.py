abda0975238cfa5ffe37697ab0d1933fd26985ff
macoid <ashwani1111995@rediff.com>
2023-02-23 23:18:40 +0530

    using requests for fetching data

--- api.py
+++ api.py
@@ -3,5 +3,7 @@ import requests
 keyword_from_user = input("Give the keyword")
 keyword_from_user = keyword_from_user.replace(' ', '')
 url = "www.amazon.in/s?k={}".format(keyword_from_user)
+data = requests.get(url)
+print(data.txt)
 
 
